85fe1548b894f11e98b63e26eb9897302e994efb
Alumno <Alumno@DAM1-PC13>
2025-03-04 09:34:10 +0100

    Estructura del Programa de FranciscoOC

--- PracticaCV_FranciscoOC/Program.cs
+++ PracticaCV_FranciscoOC/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // CONSTANTES
+
+            // VARIABLES
+
+            // ENTRADA
+            
+            // PROCESO
+
+            // SALIDA
         }
     }
 }
